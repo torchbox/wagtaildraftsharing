f075a9d59aa5da7113a1e1b6a5dbd043585386d3
Steve Jalim <stevejalim@mozilla.com>
2024-09-23 16:51:53 +0100

    Make draftsharing verbose names customisable

--- wagtaildraftsharing/models.py
+++ wagtaildraftsharing/models.py
@@ -5,10 +5,15 @@ from django.db import models
 from django.urls import reverse
 from django.utils.html import format_html
 
+from . import settings as draftsharing_settings
+
 
 class WagtaildraftsharingLink(models.Model):
     key = models.UUIDField(
-        unique=True, default=uuid.uuid4, editable=False, primary_key=False
+        unique=True,
+        default=uuid.uuid4,
+        editable=False,
+        primary_key=False,
     )
     revision = models.OneToOneField(
         "wagtailcore.Revision",
@@ -17,7 +22,9 @@ class WagtaildraftsharingLink(models.Model):
     )
     is_active = models.BooleanField(default=True)
     active_until = models.DateTimeField(
-        null=True, blank=True, help_text="Leave blank for no expiry"
+        null=True,
+        blank=True,
+        help_text="Leave blank for no expiry",
     )
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -28,12 +35,25 @@ class WagtaildraftsharingLink(models.Model):
         editable=False,
     )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Set the verbose names from settings,
+        # in a way that doesn't trigger migrations
+        self._meta.verbose_name = (
+            draftsharing_settings.WAGTAIL_DRAFTSHARING_VERBOSE_NAME
+        )
+        self._meta.verbose_name_plural = (
+            draftsharing_settings.WAGTAIL_DRAFTSHARING_VERBOSE_NAME_PLURAL
+        )
+
     def __str__(self):
         return f"Revision {self.revision_id} of {self.revision.content_object}"
 
     class Meta:
-        verbose_name = "Draftsharing Link"
-        verbose_name_plural = "Draftsharing Links"
+        # These may be changed in __init__ from settings
+        verbose_name = "Sharing Link"
+        verbose_name_plural = "Sharing Links"
 
     @property
     def url(self):
--- wagtaildraftsharing/settings.py
+++ wagtaildraftsharing/settings.py
@@ -3,3 +3,12 @@ from django.conf import settings
 WAGTAIL_DRAFTSHARING_ADMIN_MENU_POSITION = getattr(
     settings, "WAGTAIL_DRAFTSHARING_ADMIN_MENU_POSITION", 200
 )
+
+
+WAGTAIL_DRAFTSHARING_VERBOSE_NAME = getattr(
+    settings, "WAGTAIL_DRAFTSHARING_VERBOSE_NAME", "Draftsharing Link"
+)
+
+WAGTAIL_DRAFTSHARING_VERBOSE_NAME_PLURAL = getattr(
+    settings, "WAGTAIL_DRAFTSHARING_VERBOSE_NAME_PLURAL", "Draftsharing Links"
+)
